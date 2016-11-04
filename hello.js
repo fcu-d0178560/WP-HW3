0869a00db5d12dabfdb03df9bb83f787bd0e3015
BuildTools <unconfigured@null.spigotmc.org>
2016-11-04 21:29:01 +0800

    新增第一版作業

---
+++ hello.js
@@ -0,0 +1,10 @@
+﻿getTempature()
+function getTempature() {
+    var input;
+    while (input == null || input == "") {
+		input = prompt('請輸入攝氏溫度');
+    }
+	alert('Debug : '+input);
+	var tempature = input*9/5+32;
+	alert('華氏溫度 : '+tempature);
+}
